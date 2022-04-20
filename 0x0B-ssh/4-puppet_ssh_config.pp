41cda475dd9162a902f42dfe2b37f5aa9645535a
lperezcas16 <lperezcas@unbosque.edu.co>
2022-04-20 12:31:37 -0500

    [ADD] file

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# ssh config with puppet
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
