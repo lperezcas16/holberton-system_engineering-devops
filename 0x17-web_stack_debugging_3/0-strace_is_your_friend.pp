2932784493dd5b9f1f0e6772987cc4add1f2d7a0
lperezcas16 <lperezcas@unbosque.edu.co>
2022-05-02 10:40:50 -0500

    [ADD] files

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fixing error 500 using puppet config
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+# 0x17. Web stack debugging #3
+[Link to the project](https://intranet.hbtn.io/projects/293)
